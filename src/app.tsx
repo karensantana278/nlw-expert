7621c122527ac3bd3ecfbe7f19d8846f8597376d
karensantana278 <karensantana278@gmail.com>
2024-02-08 10:19:38 -0300

    add responsive and function to delete note

--- src/app.tsx
+++ src/app.tsx
@@ -38,6 +38,16 @@ export function App() {
     localStorage.setItem('notes', JSON.stringify(notesArray))
   }
 
+  function onNoteDeleted(id: string){
+    const notesArray = notes.filter(note => {
+      return note.id !== id
+    })
+
+    setNotes(notesArray)
+
+    localStorage.setItem('notes', JSON.stringify(notesArray))
+  }
+
   function handleSearch(event: ChangeEvent<HTMLInputElement>){
     const query = event.target.value
 
@@ -47,7 +57,7 @@ export function App() {
   const filteredNotes = search !== '' ? notes.filter(note => note.description.toLocaleLowerCase().includes(search.toLocaleLowerCase())) : notes
 
   return (
-    <div className="mx-auto max-w-6xl my-12 space-y-6">
+    <div className="mx-auto max-w-6xl my-12 space-y-6 px-5 lg:px-0">
       <img src={logo} alt="nlw experts" />
 
       <form className="w-full">
@@ -60,12 +70,12 @@ export function App() {
 
       <div className="h-px bg-slate-700" />
 
-      <div className="grid grid-cols-3 auto-rows-[250px] gap-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 auto-rows-[250px] gap-6">
 
         <NewNoteCard  onNoteCreated={onNoteCreated} />
 
 
-        { filteredNotes.map(note => <NoteCard key={note.id} note={note} /> )}
+        { filteredNotes.map(note => <NoteCard key={note.id} note={note} onNoteDeleted={onNoteDeleted}/> )}
 
       </div>
 
